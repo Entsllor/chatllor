087edf8759ad80d7a341016a1ab49a98cf99bd12
Entsllor <slava90100@yandex.com>
2022-05-03 22:05:27 +0500

    refactor: delete unused schemas

--- backend/app/schemas/tokens.py
+++ backend/app/schemas/tokens.py
@@ -6,19 +6,6 @@ class Token(BaseScheme):
     body: str
 
 
-class AccessToken(Token):
-    type: str = "Bearer"
-
-
-class RefreshToken(Token):
-    user_id: int = None
-
-
-class AuthTokens(BaseScheme):
-    access_token: AccessToken
-    refresh_token: RefreshToken
-
-
 class AccessTokenOut(BaseScheme):
     access_token: str
     token_type: str = "bearer"
