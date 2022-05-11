f00bc54b0714ff7a2ca8e79dfc118584933e9cf5
Entsllor <slava90100@yandex.com>
2022-05-11 23:58:02 +0500

    feat(frontend): add searching chat by name

--- frontend/app/src/components/chat-search/ChatSearch.tsx
+++ frontend/app/src/components/chat-search/ChatSearch.tsx
@@ -4,20 +4,38 @@ import ChatService from "../../services/chat";
 
 const ChatSearch: React.FC<{}> = (props) => {
   const [chats, setChats] = useState<Chat[]>([])
+  const [nameFilter, setNameFilter] = useState<string>('')
+  const [filteredChats, setFilteredChats] = useState<Chat[]>([])
 
   useEffect(() => {
-    ChatService.fetchChats().then(response => setChats(response.data));
+    ChatService.fetchChats().then(response => {
+      setChats(response.data)
+    });
   }, [])
+
+  useEffect(() => {
+    let newFilteredChats = chats;
+    newFilteredChats = newFilteredChats.filter(
+      (chat) => chat.name.toLowerCase().includes(nameFilter.toLowerCase())
+    )
+    setFilteredChats(newFilteredChats)
+  }, [nameFilter]);
 
   return (
     <div className='ChatSearch'>
-      {chats.map((chat) => (
+      <input type='text'
+             value={nameFilter}
+             onChange={event => setNameFilter(event.target.value)}
+             placeholder='Search by name'
+             className='form-control mb-3'/>
+
+      {filteredChats.map((chat) => (
         <div key={chat.id} className="card card-header mb-3">
           {chat.name}
         </div>
       ))}
     </div>
-  )
+  );
 };
 
 export default ChatSearch;
